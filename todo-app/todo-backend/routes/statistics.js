d3875ac6177436790c879fcf0eba5f7c0de41bfc
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2022-04-01 23:50:06 +0300

    Finish exercises for part b

--- todo-app/todo-backend/routes/statistics.js
+++ todo-app/todo-backend/routes/statistics.js
@@ -1,17 +1,12 @@
+const redis = require("../redis");
 const express = require("express");
 const router = express.Router();
 
-const configs = require("../util/config");
-
-let visits = 0;
-
-/* GET index data. */
+/* GET statistics data. */
 router.get("/", async (req, res) => {
-  visits++;
-
+  const addedTodos = +(await redis.getAsync("added_todos"));
   res.send({
-    ...configs,
-    visits,
+    added_todos: addedTodos || 0,
   });
 });
 
